0e9dbf54ba4aab48f2e5c9fd156d91291146698d
David Seiferth <davidseiferth@Davids-MacBook-Pro-4.local>
2023-08-07 11:18:45 +0100

    mapping

---
+++ mapping_survival.py
@@ -0,0 +1,78 @@
+# mapping
+map_age_at_diagnosis = {
+    0 : 'All ages',
+    3 : '00-44',
+    4 : '45-54',
+    6 : '55-64',
+    8 : '65-74',
+    9 : '75+'
+}
+
+
+map_statistic_type = {
+    1: 'Relative survival',
+    2: 'Number of cases',
+    3: 'SE of relative survival',
+}
+
+map_site = {0 : 'All Sites',
+1 : 'Oral Cavity and Pharynx',
+2 : 'Esophagus',
+3 : 'Stomach',
+4 : 'Colon and Rectum',
+5 : 'Liver and Intrahepatic Bile Duct',
+6 : 'Gallbladder',
+7 : 'Pancreas',
+8 : 'Larynx',
+9 : 'Lung and Bronchus',
+10 : 'Bones and Joints',
+11 : 'Soft Tissue including Heart',
+12 : 'Melanoma of the Skin',
+13 : 'Breast',
+14 : 'Cervix Uteri',
+15 : 'Corpus and Uterus, NOS',
+16 : 'Ovary',
+17 : 'Prostate',
+18 : 'Testis',
+19 : 'Urinary Bladder',
+20 : 'Kidney and Renal Pelvis',
+21 : 'Eye and Orbit',
+22 : 'Brain and Other Nervous System',
+23 : 'Thyroid',
+24 : 'Hodgkin Lymphoma',
+25 : 'Non-Hodgkin Lymphoma',
+26 : 'Myeloma',
+27 : 'Leukemia',
+28 : 'Lymphocytic Leukemia',
+29 : 'Acute Lymphocytic Leukemia',
+30 : 'Chronic Lymphocytic Leukemia',
+31 : 'Myeloid and Monocytic Leukemia',
+32 : 'Acute Myeloid Leukemia',
+33 : 'Acute Monocytic Leukemia',
+34 : 'Chronic Myeloid Leukemia',
+35 : 'Mesothelioma',
+36 : 'Kaposi Sarcoma',}
+
+
+map_year_of_diagnosis = {
+    0 : '1975-1977',
+    1 : '1978-1980',
+    2 : '1981-1983',
+    3 : '1984-1986',
+    4 : '1987-1989',
+    5 : '1990-1992',
+    6 : '1993-1995',
+    7 : '1996-1998',
+    8 : '1999-2001',
+    9 : '2002-2004',
+    10 : '2005-2007',
+    11 : '2008-2012',
+    12 : '2013-2019',
+}   
+
+
+map_sex = {
+    0 : 'Male and female',
+    1 : 'Male',
+    2 : 'Female'
+}
